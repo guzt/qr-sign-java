0300c6ae26cf772d0c8d473ebc7b44a4e55f5448
guzt <arjun@iiitd.com>
2013-01-28 15:46:42 +0530

    added license

--- src/com/iiitd/qre/Sign.java
+++ src/com/iiitd/qre/Sign.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013 Arjun Asthana
+ * This file is part of qr-gen-java.
+ *
+ * qr-gen-java is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * qr-gen-java is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Foobar. If not, see http://www.gnu.org/licenses/.
+ */
+
 package com.iiitd.qre;
 
 import java.io.IOException;
